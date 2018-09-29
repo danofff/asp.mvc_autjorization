dc920abc43ed7fba1bdef03916f0419039b405b4
Вершковд <@214_4>
2018-09-29 10:14:30 +0600

    Add project files.

---
+++ webAppl/Infastructure/AppIdentyDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using webAppl.Models;
+
+namespace webAppl.Infastructure
+{
+    public class AppIdentyDbContext:IdentityDbContext<AppUser>
+    {
+        public AppIdentyDbContext():base("name=IdentityDb")
+        {   
+        }
+
+        static AppIdentyDbContext()
+        {
+            Database.SetInitializer<AppIdentyDbContext>( new IdentityDbInit());
+        }
+
+        public static AppIdentyDbContext Create()
+        {
+            return new AppIdentyDbContext();
+        }
+    }
+
+    public class IdentityDbInit:DropCreateDatabaseIfModelChanges<AppIdentyDbContext>
+    {
+        protected override void Seed(AppIdentyDbContext context)
+        {
+            PerformInitialSeed(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSeed(AppIdentyDbContext context)
+        {
+
+        }
+    }
+}
